8a0c68c580609e5ab2d7642999b141dc137f42f4
Giuseppe Chianese <giuseppe.chianese@anritsu.com>
2020-12-10 17:14:55 +0100

    Ngrx Store with LocalStorage

--- src/app/root-store/root-reducer.ts
+++ src/app/root-store/root-reducer.ts
@@ -1,6 +1,7 @@
-import {ActionReducerMap, MetaReducer} from '@ngrx/store';
+import {ActionReducer, ActionReducerMap, MetaReducer} from '@ngrx/store';
 import {State} from '@root-store/state';
 import {environment} from '../../environments/environment';
+import {localStorageSync} from 'ngrx-store-localstorage';
 
 
 export interface AppState {
@@ -8,4 +9,7 @@ export interface AppState {
 
 export const reducers: ActionReducerMap<AppState> = {};
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({rehydrate : true , keys: ['auth']})(reducer);
+}
+export const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
